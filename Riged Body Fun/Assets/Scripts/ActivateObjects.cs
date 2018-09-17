b02745c65d645b1cefcac2d0f8b912ca537826cc
Arniox <nikkdiehl@gmail.com>
2018-09-17 15:20:34 +1200

    Tests

---
+++ Riged Body Fun/Assets/Scripts/ActivateObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObjects : MonoBehaviour {
+
+	[SerializeField] bool activated = true;
+
+	void Start () {
+		// Use this for initialization
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.activateGivenObject(activated);
+	}
+
+	public void activateGivenObject(bool yesNo){
+		gameObject.SetActive(yesNo);
+	}
+}
